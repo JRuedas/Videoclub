3895123802a31db65d401aec7f5d13c073da8aae
adriTorres <adri.torres.m96@gmail.com>
2018-06-20 21:34:49 +0200

    Movie model

--- videoclubUPM/videoclub/models.py
+++ videoclubUPM/videoclub/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class movie(models.Model):
+    titulo = models.CharField(max_length=200, null=False)
+    descripcion = models.CharField(max_length=2000, null=False)
+    fecha = models.DateField(null=False)
+    director = models.CharField(max_length=100, null=False)
+    url_portada = models.CharField(max_length=300, null=False)
+    valoracion = models.DecimalField(max_digits=4, decimal_places=2, null=False)
+    url_video = models.CharField(max_length=300)
+    presupuesto = models.DecimalField(max_digits=100, decimal_places=2)
+    ingresos = models.DecimalField(max_digits=100, decimal_places=2)
+    lenguaje = models.CharField(max_length=100)
