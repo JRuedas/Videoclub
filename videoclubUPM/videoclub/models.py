21bc16227e460cf74dc7c375a2ce53dc7b2916a8
adriTorres <adri.torres.m96@gmail.com>
2018-06-21 02:39:13 +0200

    Rest framework and Serializers added, Models and some Css fixed, and add_film new interface.

--- videoclubUPM/videoclub/models.py
+++ videoclubUPM/videoclub/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 
-class movie(models.Model):
-    titulo = models.CharField(max_length=200, null=False)
-    descripcion = models.CharField(max_length=2000, null=False)
-    fecha = models.DateField(null=False)
+# https://docs.djangoproject.com/en/2.0/topics/db/models/
+# https://docs.djangoproject.com/en/2.0/topics/db/examples/many_to_many/
+
+class Movie(models.Model):
+    id_movie = models.IntegerField()
+    title = models.CharField(max_length=200, null=False)
+    overview = models.CharField(max_length=2000, null=False)
+    date = models.DateField(null=False)
     director = models.CharField(max_length=100, null=False)
-    url_portada = models.CharField(max_length=300, null=False)
-    valoracion = models.DecimalField(max_digits=4, decimal_places=2, null=False)
+    url_poster = models.CharField(max_length=300, null=False)
+    vote_average = models.DecimalField(max_digits=4, decimal_places=2, null=False)
     url_video = models.CharField(max_length=300)
-    presupuesto = models.DecimalField(max_digits=100, decimal_places=2)
-    ingresos = models.DecimalField(max_digits=100, decimal_places=2)
-    lenguaje = models.CharField(max_length=100)
+    budget = models.DecimalField(max_digits=100, decimal_places=2)
+    revenue = models.DecimalField(max_digits=100, decimal_places=2)
+    original_language = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Cast(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
---
+++ videoclubUPM/videoclub/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from videoclub.models import Movie, Cast
+
+# http://www.django-rest-framework.org/api-guide/serializers/
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ('id_movie', 'title', 'overview', 'date', 'director', 'url_poster', 'vote_average', 'url_video', 'budget', 'revenue', 'original', 'status')
+
+
+class CastSerializer(serializers.ModelSerializer):
+
+    movies = MovieSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cast
+        fields = ('name', 'movies')
